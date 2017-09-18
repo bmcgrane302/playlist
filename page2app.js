6bd216eff4d9c2cc16ed4919e5ed2fa828119896
bmcgrane302 <bmcgrane302@yahoo.com>
2017-09-17 17:59:38 -0700

    changed page 2 js

--- page2app.js
+++ page2app.js
@@ -1,10 +1,12 @@
 console.log("test");
 $(document).ready(function() {
-  var coverArt = [0, 1, 2, 3, 4];
-  for (var i = 0; i < 5; i++) {
-    albumCovers(coverArt[i]);
+$.get('https://lit-fortress-6467.herokuapp.com/object/', function(data) {
+  console.log(data.results.length);
+
+  for (var i = 0; i < data.results.length; i++) {
+    albumCovers(i);
   }
-   //console.log(data);
+
   //console.log(data.results[i].cover_art);
 
   function albumCovers(num) {
@@ -41,65 +43,5 @@ $(document).ready(function() {
     });
 
   }
+  });
 });
-
-
-
-
-
-// console.log("test");
-// $(document).ready(function() {
-//
-//   $.get('https://lit-fortress-6467.herokuapp.com/object/', function(data) {
-//     console.log(data.results.length);
-//     //console.log(data.results[0].cover_art);
-//     /////////start of album
-//     function albumCovers() {
-//       for (var i = 0; i < data.results.length; i++) {
-//
-//         var albumList = document.getElementsByClassName('albumList')[0];
-//         var img = document.createElement('img');
-//         img.className = "albumCov";
-//         img.id = "album"+i;
-//         img.src = `images/${data.results[i].cover_art}`;
-//         console.log(img);
-//         albumList.append(img);
-//     ///added click
-//
-//         $("#album"+i).click(function() {
-//           var textBox = document.getElementsByClassName('textBox')[0];
-//           var p = document.createElement('p');
-//           p.innerHTML = data.results[0].artist;
-//           textBox.append(p);
-//
-//         })
-//     //bottom of click .results["0"].artist
-//       }
-//     }
-//     albumCovers();
-//     ////end album
-//     // $('#album0').click(function(){
-//     //   console.log("album test");
-//     //     var textBox = document.getElementsByClassName('textBox')[0];
-//     //     var p = document.createElement('p');
-//     //     p.innerHTML = data.results[0].artist+ ": "+data.results[0].title;
-//     //     textBox.append(p);
-//     // });
-//     // $('#album1').click(function(){
-//     //   console.log("album test");
-//     //     var textBox = document.getElementsByClassName('textBox')[0];
-//     //     var p = document.createElement('p');
-//     //     p.innerHTML = data.results[1].artist+ ": "+data.results[1].title;
-//     //     textBox.append(p);
-//     // });
-//
-//     /////clear button
-//     $('#clear').click(function() {
-//       $('.textBox').empty();
-//     });
-//     ///////post button
-//
-//
-//   });
-//
-// });
